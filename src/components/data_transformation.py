dc5480cc8f44a295625b514465bec533fb9843fb
vickey karthik <vickeykarthik@vickeys-MacBook-Air.local>
2026-05-03 12:40:47 +0530

    update notebook

--- src/components/data_transformation.py
+++ src/components/data_transformation.py
@@ -28,13 +28,9 @@ class DataTransformation:
         
         '''
         try:
-            numerical_columns = ["writing_score", "reading_score"]
+            numerical_columns = ["transaction_id", 'user_id', 'amount', 'hour', 'device_risk_score', 'ip_risk_score', 'is_fraud']
             categorical_columns = [
-                "gender",
-                "race_ethnicity",
-                "parental_level_of_education",
-                "lunch",
-                "test_preparation_course",
+                ['transaction_type', 'merchant_category', 'country']
             ]
 
             num_pipeline= Pipeline(
@@ -86,7 +82,6 @@ class DataTransformation:
             preprocessing_obj=self.get_data_transformer_object()
 
             target_column_name="math_score"
-            numerical_columns = ["writing_score", "reading_score"]
 
             input_feature_train_df=train_df.drop(columns=[target_column_name],axis=1)
             target_feature_train_df=train_df[target_column_name]
@@ -95,7 +90,7 @@ class DataTransformation:
             target_feature_test_df=test_df[target_column_name]
 
             logging.info(
-                f"Applying preprocessing object on training dataframe and testing dataframe."
+                "Applying preprocessing object on training dataframe and testing dataframe."
             )
 
             input_feature_train_arr=preprocessing_obj.fit_transform(input_feature_train_df)
@@ -106,7 +101,7 @@ class DataTransformation:
             ]
             test_arr = np.c_[input_feature_test_arr, np.array(target_feature_test_df)]
 
-            logging.info(f"Saved preprocessing object.")
+            logging.info("Saved preprocessing object.")
 
             save_object(
 
